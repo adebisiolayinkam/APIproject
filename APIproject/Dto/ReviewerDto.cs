70683b6c837c002219ec75d7e83508604e1c4c81
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2023-01-31 09:53:49 +0100

    Add project files.

---
+++ APIproject/Dto/ReviewerDto.cs
@@ -0,0 +1,12 @@
+﻿using APIproject.Models;
+
+namespace APIproject.Dto
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ APIproject/Helper/MappingProfiles.cs
@@ -0,0 +1,25 @@
+﻿using APIproject.Dto;
+using APIproject.Models;
+using AutoMapper;
+
+namespace APIproject.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+            CreateMap<PokemonDto, Pokemon>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+            CreateMap<Country, CountryDto>();
+            CreateMap<CountryDto, Country>();
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<OwnerDto, Owner>();
+            CreateMap<Review, ReviewDto>();
+            CreateMap<ReviewDto, Review>();
+            CreateMap<Reviewer, ReviewerDto>();
+            CreateMap<ReviewerDto, Reviewer>();
+        }
+    }
+}
